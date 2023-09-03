0224f3069397a9aee0d96b2464d659456f124f16
Steven Pacheco P <stevenpach10@gmail.com>
2023-09-03 15:26:31 -0600

    Add Hydra configuration files

--- Pytorch LIghtning/Hydra/train.py
+++ Pytorch LIghtning/Hydra/train.py
@@ -13,24 +13,28 @@ from tqdm import tqdm
 from torch.utils.data import random_split
 import pytorch_lightning as pl
 import torchmetrics
+from config.config import Configuration
 from model import NN
 from dataset import MnistDataModule
-import config
 from callbacks import MyPrintingCallback, EarlyStopping
 import torch.multiprocessing
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 from pytorch_lightning.profilers import PyTorchProfiler
+import hydra
+from hydra.core.config_store import ConfigStore
 
-
-
-if __name__ == "__main__":
+cs = ConfigStore.instance()
+cs.store(name="mnist_config", node=Configuration)
+@hydra.main(config_path="config", config_name="config")
+def main(config : Configuration):
+    print(config)
     torch.multiprocessing.set_sharing_strategy('file_system')
     logger = TensorBoardLogger('tb_logs', name="mnist_model_v1")
     #logger_wand = WandbLogger(name='wand_logs', project='Pytorch LIghtning')
     # initialise the wandb logger and name your wandb project
-    myDataModule = MnistDataModule(data_dir=config.DATA_DIR, batch_size=config.BATCH_SIZE, num_workers=config.NUM_WORKERS)
+    myDataModule = MnistDataModule(data_dir=config.dataset.DATA_DIR, batch_size=config.train.BATCH_SIZE, num_workers=config.train.NUM_WORKERS)
     # Initialize network
-    model = NN(input_size=config.INPUT_SIZE, num_classes=config.NUM_CLASSES, learning_rate=config.LEARNING_RATE).to(config.ACCELERATOR)
+    model = NN(input_size=config.model.INPUT_SIZE, num_classes=config.model.NUM_CLASSES, learning_rate=config.train.LEARNING_RATE).to(config.train.ACCELERATOR)
     profiler = PyTorchProfiler(
         on_trace_ready=torch.profiler.tensorboard_trace_handler("tb_logs/profiler2"),
         scheduler = torch.profiler.schedule(skip_first=0, wait=0, warmup=0, active=200)
@@ -38,12 +42,15 @@ if __name__ == "__main__":
     trainer = pl.Trainer(
         profiler=profiler,
         logger = logger,
-        accelerator=config.ACCELERATOR,
+        accelerator=config.train.ACCELERATOR,
         min_epochs=1,
-        max_epochs=config.NUM_EPOCHS,
-        precision=config.PRECISION,
+        max_epochs=config.train.NUM_EPOCHS,
+        precision=config.train.PRECISION,
         callbacks=[MyPrintingCallback(), EarlyStopping(monitor='val_loss')])
     trainer.fit(model, myDataModule)
     trainer.validate(model, myDataModule)
     trainer.test(model, myDataModule)
     #wandb.finish()
+
+if __name__ == "__main__":
+    main()
